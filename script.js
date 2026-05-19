16b8c952d06ea2ac6584110a82f0916a9b240f81
JEISON GARCIA <jeysonk21@gmail.com>
2026-05-19 16:33:38 -0500

    Update script.js

--- script.js
+++ script.js
@@ -27,3 +27,20 @@ projectsBtn.addEventListener('click', (e) => {
   });
 
 });
+const hiddenElements = document.querySelectorAll('.hidden');
+
+const observer = new IntersectionObserver((entries) => {
+
+  entries.forEach((entry) => {
+
+    if(entry.isIntersecting){
+
+      entry.target.classList.add('show');
+
+    }
+
+  });
+
+});
+
+hiddenElements.forEach((el) => observer.observe(el));
